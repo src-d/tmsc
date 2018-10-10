db81570cfcf97df4bca00edf16f79af8bfb99035
Konstantin Slavnov <kslavnov@gmail.com>
2018-10-10 15:02:20 +0300

    Add long description to setup()
    Signed-off-by: Konstantin Slavnov <kslavnov@gmail.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
+import os.path
 from setuptools import setup, find_packages
 
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="tmsc",
     description="Part of source{d}'s stack for machine learning on source "
                 "code. Provides API and tools to detect topics in Git "
                 "repositories.",
-    version="0.1.5-alpha",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="0.1.6-alpha",
     license="Apache Software License",
     author="source{d}",
     author_email="machine-learning@sourced.tech",
